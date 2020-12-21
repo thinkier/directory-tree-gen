6fdcd235cc83a08985692cd65f43c1e00016af2c
thinkier <26728734+thinkier@users.noreply.github.com>
2020-12-21 17:02:49 +1100

    customize directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::env;
+extern crate argh;
+
+use std::{env, process};
 use std::error::Error;
 use std::io::stdout;
+use std::path::PathBuf;
+
+use argh::FromArgs;
 
 use crate::directory::generate_text;
 use crate::excludes::Excludes;
@@ -8,8 +13,30 @@ use crate::excludes::Excludes;
 mod directory;
 mod excludes;
 
+#[derive(FromArgs)]
+/// Directory tree generator
+struct MainArgs {
+	/// the directory to generate a tree for, defaults to current directory.
+	#[argh(option)]
+	dir: Option<String>
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+	let args: MainArgs = argh::from_env();
+
+	let path = args.dir
+		.map(|dir| {
+			match PathBuf::from(&dir).canonicalize() {
+				Ok(x) => x,
+				Err(_) => {
+					eprintln!("File/folder not found!");
+					process::exit(1);
+				}
+			}
+		})
+		.unwrap_or_else(|| env::current_dir().unwrap());
+
 	let excludes = Excludes::from_ignores();
 
-	generate_text(&mut stdout(), &env::current_dir().unwrap(), &excludes)
+	generate_text(&mut stdout(), &path, &excludes)
 }
